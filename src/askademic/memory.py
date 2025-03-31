af6e865553b32ac5e51794b044c966ee03a2c875
bernomone <mone.berna@gmail.com>
2025-03-31 23:06:55 +0200

    update memory class with pre commit hooks

--- src/askademic/memory.py
+++ src/askademic/memory.py
@@ -1,5 +1,6 @@
 import logging
-from typing import List, Dict
+from typing import Dict, List
+
 from pydantic_ai.messages import ModelMessage
 
 logging.basicConfig(level=logging.INFO, filename="logs.txt")
@@ -7,20 +8,19 @@ logger = logging.getLogger(__name__)
 
 
 class Memory:
-
     def __init__(self, max_request_tokens=2e5):
         self._max_request_tokens = max_request_tokens
         self._message_history: List[Dict[str, int | ModelMessage]] = []
 
-    def __len__(self):  
+    def __len__(self):
         return len(self._message_history)
-    
+
     def __iter__(self):
         return iter(self._message_history)
-    
+
     def __getitem__(self, index):
         return self._message_history[index]
-    
+
     def _prune_history(self):
         """Prune the message history to fit within the token limit."""
         total_tokens = self.get_total_tokens()
@@ -37,22 +37,20 @@ class Memory:
         """Add a message to the memory."""
         total_tokens = self.get_total_tokens()
         message_tokens = message_tokens - total_tokens
-        self._message_history.append({
-            "message_tokens": message_tokens,
-            "message": message
-        })
-    
+        self._message_history.append(
+            {"message_tokens": message_tokens, "message": message}
+        )
+
         total_tokens = self.get_total_tokens()
         logger.info(f"*** Adding messages, current total tokens: {total_tokens}")
-
 
     def get_messages(self):
         """Get the message history."""
         messages = []
         for m in self._message_history:
-            messages+=m["message"]
-        return messages 
-    
+            messages += m["message"]
+        return messages
+
     def clear_history(self):
         """Clear the message history."""
         self._message_history = []
