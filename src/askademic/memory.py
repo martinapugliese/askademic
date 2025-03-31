9174aa6acf5fee5735157a1acea3f308e10466a8
bernomone <mone.berna@gmail.com>
2025-03-31 23:05:37 +0200

    update memory class

--- src/askademic/memory.py
+++ src/askademic/memory.py
@@ -5,9 +5,10 @@ from pydantic_ai.messages import ModelMessage
 logging.basicConfig(level=logging.INFO, filename="logs.txt")
 logger = logging.getLogger(__name__)
 
+
 class Memory:
 
-    def __init__(self, max_request_tokens=5e5):
+    def __init__(self, max_request_tokens=2e5):
         self._max_request_tokens = max_request_tokens
         self._message_history: List[Dict[str, int | ModelMessage]] = []
 
@@ -21,41 +22,43 @@ class Memory:
         return self._message_history[index]
     
     def _prune_history(self):
-        total_tokens = self._message_history[-1]["total_tokens"]
+        """Prune the message history to fit within the token limit."""
+        total_tokens = self.get_total_tokens()
         while total_tokens > self._max_request_tokens:
-            first_message_tokens = self._message_history.pop(0)["total_tokens"]
+            _ = self._message_history.pop(0)
             if len(self._message_history) == 0:
                 break
-            for m in self._message_history:
-                m["total_tokens"] -= first_message_tokens
-            total_tokens = self._message_history[-1]["total_tokens"]
+            total_tokens = self.get_total_tokens()
 
+        total_tokens = self.get_total_tokens()
         logger.info(f"*** Pruned messages, current total tokens: {total_tokens}")
 
-    def add_message(self, total_tokens: int, message: ModelMessage):
+    def add_message(self, message_tokens: int, message: ModelMessage):
+        """Add a message to the memory."""
+        total_tokens = self.get_total_tokens()
+        message_tokens = message_tokens - total_tokens
+        self._message_history.append({
+            "message_tokens": message_tokens,
+            "message": message
+        })
+    
+        total_tokens = self.get_total_tokens()
+        logger.info(f"*** Adding messages, current total tokens: {total_tokens}")
 
-        if total_tokens > self.get_last_message_tokens():
-
-            logger.info(f"*** Adding messages, current total tokens: {total_tokens}")
-            self._message_history.append({
-                "total_tokens": total_tokens,
-                "message": message
-            })
-        self._prune_history()
 
     def get_messages(self):
+        """Get the message history."""
         messages = []
         for m in self._message_history:
             messages+=m["message"]
         return messages 
     
     def clear_history(self):
+        """Clear the message history."""
         self._message_history = []
 
-    def get_last_message(self): 
-        return self._message_history[-1]["message"] if self._message_history else None
-    
-    def get_last_message_tokens(self):  
-        return self._message_history[-1]["total_tokens"] if self._message_history else 0
-
-
+    def get_total_tokens(self):
+        """Get the total number of tokens in the message history."""
+        if len(self._message_history) == 0:
+            return 0
+        return sum([m["message_tokens"] for m in self._message_history])
