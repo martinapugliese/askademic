c275d8dec65f6c3322be56d5865ffc1588b14a16
bernomone <mone.berna@gmail.com>
2025-04-06 15:09:11 +0200

    comment about allower memory

--- src/askademic/main.py
+++ src/askademic/main.py
@@ -66,7 +66,9 @@ async def ask_me():
                 allower = await allower_agent.run(
                     USER_PROMPT_ALLOWER_TEMPLATE.format(question=user_question),
                     usage_limits=UsageLimits(request_limit=20),  # limit to 20 requests
-                    message_history=memory.get_messages()[-2:],
+                    message_history=memory.get_messages()[
+                        -2:
+                    ],  # only the last 2 messages to keep the context, with 1 it loses it sometimes
                 )
 
                 if allower.data.is_scientific:
