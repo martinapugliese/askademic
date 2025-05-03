32424192870420cd7810bc6565e696cca1e253ca
martinapugliese <m.letitbe@gmail.com>
2025-05-03 18:41:50 +0100

    evals(allower): added test cases

--- evals/eval_allower.py
+++ evals/eval_allower.py
@@ -13,26 +13,32 @@ class AllowerTestCase:
 eval_cases = [
     AllowerTestCase("What is the latest research on quantum computing?", True),
     AllowerTestCase("Hello, how are you?", False),
+    AllowerTestCase("Can you summarize the latest papers on AI?", True),
+    AllowerTestCase("Tell me a joke about physics.", False),
+    AllowerTestCase("What are the implications of quantum entanglement?", True),
+    AllowerTestCase("What is the meaning of life?", False),
 ]
 
 
 async def run_evals():
 
+    c_passed, c_failed = 0, 0
     for case in eval_cases:
 
         print(f"Evaluating case: {case.question}")
         response = await allower_agent.run(
             USER_PROMPT_ALLOWER_TEMPLATE.format(question=case.question)
         )
-        print(f"Response: {response}")
 
         if response.output.is_scientific != case.is_scientic_gt:
             print(f"Test failed for question: {case.question}")
-            print(
-                f"Expected: {case.is_scientic_gt}, Got: {response.output.is_scientific}"
-            )
+            c_failed += 1
         else:
-            print(f"Test passed for question: {case.question}")
+            c_passed += 1
+
+    print(f"Total cases: {len(eval_cases)}")
+    print(f"Passed: {c_passed}")
+    print(f"Failed: {c_failed}")
 
 
 def main():
